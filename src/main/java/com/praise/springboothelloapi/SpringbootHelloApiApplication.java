e872e2b1903fa5936516fb8fa1982a3ea0ee01d7
Praizel <praise_clement@yahoo.com>
2026-06-18 16:56:42 +0100

    Created first Spring Boot hello API

---
+++ src/main/java/com/praise/springboothelloapi/SpringbootHelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.praise.springboothelloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHelloApiApplication.class, args);
+    }
+
+}
