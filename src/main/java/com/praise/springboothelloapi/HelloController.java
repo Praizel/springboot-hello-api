3d5eac80a12f75db1467df6cfe2e19e9869fe437
Praizel <praise_clement@yahoo.com>
2026-06-21 13:53:24 +0100

    Added about endpoint and path variable greeting

--- src/main/java/com/praise/springboothelloapi/HelloController.java
+++ src/main/java/com/praise/springboothelloapi/HelloController.java
@@ -1,6 +1,8 @@
 package com.praise.springboothelloapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import  org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,25 @@ public class HelloController {
 	public String hello() {
         return "Hello Praise, welcome to Spring Boot!";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "I am learning Spring Boot and becoming a Software Engineer.";
+    }
+
+
+    @GetMapping("/hello/{name}")
+    public String name(@PathVariable String name) {
+        return "Hello " + name + ", " + " " + "welcome to Spring Boot and becoming a Software.";
+    }
+
 }
+
+
+
+
+
+
+
+
 
